5ad055014d8da0f040670e8228e0d8b850753928
unknown <gemechubulti11@gmail.com>
2024-06-04 15:37:50 +0300

    data

--- routes/workspaceRoutes.js
+++ routes/workspaceRoutes.js
@@ -2,9 +2,9 @@ const express = require("express");
 const workspacesControllers = require("../controllers/workspaceController.js");
 // const middleware=require("../middleware/auth.js")
 const router = express.Router();
+//GET WORKSPACES
 router.get("/workspaces", workspacesControllers.getWorkspaces);
+//GET ACCESS TOKEN
 router.get("/", workspacesControllers.getAccessToken);
-
-
 
 module.exports = router;
